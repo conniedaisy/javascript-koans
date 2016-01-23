e8dec8c3967717f36148f4cbc7d4d0fd10a60e95
Connie Cheung <conniedcheung@gmail.com>
2016-01-22 17:00:31 -0800

    AboutFunctions fix functions as strings

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -107,9 +107,11 @@ describe("About Functions", function() {
       return a * b;
     };
     
-    expect(multiply.toString()).toBe("function(a, b) {
+    var solution = "function(a, b) {
       // An internal comment
       return a * b;
-    };");
+    };"
+
+    expect(multiply.toString()).toBe(solution);
   });    
 });
